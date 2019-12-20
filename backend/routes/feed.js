26dfbcd90dfcc7070763348db4b0b6e6721e9a7a
Dominik Bláha <dominik.blaha@hvtronix.cz>
2019-12-20 12:06:31 +0100

    validation added, before sequelize

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -10,7 +10,8 @@ router.get('/posts', feedController.getPosts);
 
 // POST /feed/post
 router.post('/post', [
-  body('title')
+  body('title').trim().isLength({ min: 5 }),
+  body('content').trim().isLength({ min: 5 })
 ], feedController.postPosts);
 
 module.exports = router;
